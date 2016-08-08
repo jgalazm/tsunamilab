f395c4a8562c10048625234a3f7833087efb371b
spereirag <sebastian.pereira@inria.cl>
2016-08-08 18:29:47 -0400

    Add scenario selection GUI, #3

--- web/app/scripts/new_gui.js
+++ web/app/scripts/new_gui.js
@@ -2,56 +2,52 @@
 
 
 
-console.log("new_gui.js");
+
+//console.log("new_gui.js");
 
 
 function init_new_gui(){
 
 
-	console.log(simulationControls);
-	console.log(simulationControls.x);
 
+	//console.log("function new_gui.js");
 
-	console.log("function new_gui.js");
-
-	$(".movable").mousedown(function(){
-
-
-
-
-		console.log("mousedown!!!");
-
-
-		$(this).mousemove(function(e){
-
-			console.log(e.pageX, e.pageY);
-			$(this).css("left", e.pageX-25);
-			$(this).css("top", e.pageY-25);
-
-		});
-		
-	});
-
-	$(".movable").mouseup(function(){
-
-		console.log("mouseup");
-
-
-
-		$(this).unbind("mousemove");
-
-	});
-
-	console.log("init_new_gui()");
+	if(paused){
+		$("#icon-play").show();
+		$("#icon-pause").hide();
+	}
+	else{
+		$("#icon-play").hide();
+		$("#icon-pause").show();
+	}
 
 
 
 	$("#btn-play").click(function(){
 		simulationControls.pause();
+
+		if(paused){
+			$("#icon-play").show();
+			$("#icon-pause").hide();
+		}
+		else{
+			$("#icon-play").hide();
+			$("#icon-pause").show();
+		}
+
 	});
 
 	$("#btn-restart").click(function(){
 		simulationControls.restart();
+	});
+
+	$("#btn-snapshot").click(function(){
+		simulationControls.snapshot();
+	});
+
+	$("#scenarios").change(function(value){
+		console.log($(this).val())
+		changeScenario($(this).val());
 	});
 
 

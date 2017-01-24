b3ff019fcb0f76b020e05a2ce4cafc3d721023d7
jgalazm <jdgalaz@uc.cl>
2017-01-24 15:34:03 -0300

    play pause controls in TsunamiController

--- web/app/scripts/tsunamiController.js
+++ web/app/scripts/tsunamiController.js
@@ -1,12 +1,32 @@
-var a = 8;
-viewer.camera.flyTo({
-  destination: Cesium.Rectangle.fromDegrees(-80-3*a,-45-3*a,-70+3*a,-35+3*a)
-});
+function TsunamiController(model, view){
+  var paused = true;
 
+  var flyTo = function(){
+    // TODO: set location as input
+    var a = 8;
+    view.viewer.camera.flyTo({
+      destination: Cesium.Rectangle.fromDegrees(-80-3*a,-45-3*a,-70+3*a,-35+3*a)
+    });
+  }
 
-function tick() {
-  d1.renderSimulation();
-  rectangle.appearance.material.uniforms.image = d1.renderScreen();
-  requestAnimationFrame(tick);
+  var tick = function() {
+    if (paused){
+      model.renderSimulation();
+      view.rectangle.appearance.material.uniforms.image = model.renderScreen();
+    }
+    requestAnimationFrame(tick);
+  }
+
+  var play = function(){
+    paused = false;
+  }
+  var pause = function(){
+    paused = true;
+  }
+  return {
+    play: play,
+    pause: pause,
+    flyTo: flyTo,
+    tick: tick
+  }
 }
-tick();

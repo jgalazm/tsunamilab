f29b526f99e7aa94b52094db9d144d168757ef93
spereirag <sebastian.pereira@inria.cl>
2017-05-23 15:36:36 -0400

    Add movement to lateral cameras to get a better 3d view

--- web/app/scripts/tsunamiView.js
+++ web/app/scripts/tsunamiView.js
@@ -69,13 +69,27 @@ var TsunamiView = function(params){
       Cesium.Cartesian3.clone(masterCamera.up, slaveCamera.up);
       Cesium.Cartesian3.clone(masterCamera.write, slaveCamera.write);
       slaveCamera.lookAtTransform(masterCamera.transform);
-      slaveCamera.rotate(slaveCamera.up, offset/180*Math.PI)
+      var height = masterCamera.positionCartographic.height;
+      var offsetFactor1 = Math.min(1, Math.pow(height/10000000, 1.5));
+      var offsetFactor2 = Math.min(1, Math.pow(height/10000000, 0.5));
+      console.log(offsetFactor1, offsetFactor2);
+      if(offset == 180)
+        offsetFactor1 = 1;
+      slaveCamera.rotate(slaveCamera.up, offsetFactor1*offset/180*Math.PI);
+      if(offset != 180)
+        slaveCamera.setView({
+            orientation: {
+                heading : Cesium.Math.toRadians(90.0), // east, default value is 0.0 (north)
+                pitch : Cesium.Math.toRadians(-90+(1-offsetFactor2)*offset/1.5),    // default value (looking down)
+                roll : Cesium.Math.toRadians(-90)                           // default value
+            }
+        });
     });
   }
 
   setSlaves(viewer.camera, viewer2.camera, viewer2, 90);
   setSlaves(viewer.camera, viewer3.camera, viewer3, 180);
-  setSlaves(viewer.camera, viewer4.camera, viewer4, 270);
+  setSlaves(viewer.camera, viewer4.camera, viewer4, -90);
 
 
 
